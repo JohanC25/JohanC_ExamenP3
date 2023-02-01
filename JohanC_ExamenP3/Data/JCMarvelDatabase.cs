13e78e60c7af0c7cde3d7573ea88309de293f184
Johan Carrasco <johanmcm@hotmail.com>
2023-02-01 08:43:14 -0500

    Creacion de JCMarvelDatabase y metodos Creacion de modelo JCMarvel

---
+++ JohanC_ExamenP3/Data/JCMarvelDatabase.cs
@@ -0,0 +1,49 @@
+﻿using SQLite;
+using JohanC_ExamenP3.Models;
+
+namespace JohanC_ExamenP3.Data
+{
+	public class JCMarvelDatabase
+	{
+		string _dbPath;
+		private SQLiteConnection conn;
+
+		public JCMarvelDatabase(string DatabasePath)
+		{
+			_dbPath = DatabasePath;
+		}
+
+		private void Init()
+		{
+			if (conn != null)
+				return;
+			conn = new SQLiteConnection(_dbPath);
+			conn.CreateTable<JCMarvel>();
+		}
+		public int AddNewMarvel(JCMarvel marvel)
+		{
+			Init();
+			if (marvel.Id != 0)
+			{
+				return conn.Update(marvel);
+			}
+			else
+			{
+				return conn.Insert(marvel);
+			}
+		}
+
+		public List<JCMarvel> GetAllMarvel()
+		{
+			Init();
+			List<JCMarvel> marvel = conn.Table<JCMarvel>().ToList();
+			return marvel;
+		}
+
+		public int DeleteMarvel(JCMarvel item)
+		{
+			Init();
+			return conn.Delete(item);
+		}
+	}
+}
---
+++ JohanC_ExamenP3/Models/JCMarvel.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace JohanC_ExamenP3.Models
+{
+	[Table("JCMarvel")]
+	public class JCMarvel
+	{
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+		public string JCNombre { get; set; }
+		public string JCDescripcion { get; set; }
+		public DateTime DateTime { get; set; }
+
+	}
+}
