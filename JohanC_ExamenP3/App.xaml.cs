58a343b5e58c47c9512ba208bbb4e97930983f61
Johan Carrasco <johanmcm@hotmail.com>
2023-02-01 08:27:28 -0500

    Add project files.

---
+++ JohanC_ExamenP3/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace JohanC_ExamenP3;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ JohanC_ExamenP3/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace JohanC_ExamenP3;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
